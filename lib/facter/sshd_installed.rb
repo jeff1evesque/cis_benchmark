12c816cf819f28e80777da48ac96d7a3058f6bea
jeff levesque <jeff1evesque@yahoo.com>
2017-02-07 18:57:27 -0500

    #74: sshd_installed.rb, add file

---
+++ lib/facter/sshd_installed.rb
@@ -0,0 +1,11 @@
+## sshd_installed.rb, 
+##
+## Note: http://wiki.apparmor.net/index.php/AppArmor_Failures#Is_AppArmor_enabled.3F
+##
+
+Facter.add(:installed_xinetd) do
+  confine :osfamily => 'Debian'
+  setcode do
+    Facter::Core::Execution.exec("if (dpkg -l openssh-server > /dev/null 2>&1); then echo 'true'; else echo 'false'; fi")
+  end
+end
