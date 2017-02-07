7d59b77e69493e2ba90cf71b7c8c0de26fbb242a
jeff levesque <jeff1evesque@yahoo.com>
2017-02-07 18:58:16 -0500

    #74: sshd_installed.rb, fix trivial typo

--- lib/facter/sshd_installed.rb
+++ lib/facter/sshd_installed.rb
@@ -3,7 +3,7 @@
 ## Note: http://wiki.apparmor.net/index.php/AppArmor_Failures#Is_AppArmor_enabled.3F
 ##
 
-Facter.add(:installed_xinetd) do
+Facter.add(:installed_sshd) do
   confine :osfamily => 'Debian'
   setcode do
     Facter::Core::Execution.exec("if (dpkg -l openssh-server > /dev/null 2>&1); then echo 'true'; else echo 'false'; fi")
