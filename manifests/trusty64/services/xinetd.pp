413f79758912b87272ae4896314d096be814180a
jeffrey levesque <jeff1evesque@yahoo.com>
2017-04-04 13:56:35 -0400

    #95: xinetd.pp, clean up syntax

--- manifests/trusty64/services/xinetd.pp
+++ manifests/trusty64/services/xinetd.pp
@@ -6,16 +6,23 @@
 
 class cis::trusty64::services::xinetd {
   ## local variables: conditionally load hiera
-  $node_reference = $node_name_value
-  $hiera_node     = hiera($node_reference, 'trusty64')
+  ##
+  ## Note: yaml keys cannot contain '.', so regsubst() is used. Likewise, the
+  ##       corresponding yaml key, implements underscores instead of '.' for
+  ##       nodes certificate name.
+  ##
+  $hiera_node = lookup([
+      regsubst($trusted['certname'], '\.', '_', 'G'),
+      'trusty64'
+  ])
 
   ## local variables: stig items
-  $cis_2_1_1 = $hiera_node['cis_2_1_1']
-  $cis_2_1_2 = $hiera_node['cis_2_1_2']
-  $cis_2_1_3 = $hiera_node['cis_2_1_3']
-  $cis_2_1_4 = $hiera_node['cis_2_1_4']
-  $cis_2_1_5 = $hiera_node['cis_2_1_5']
-  $cis_2.1.10 = $hiera_node['cis_2.1.10']
+  $cis_2_1_1  = $hiera_node['cis_2_1_1']
+  $cis_2_1_2  = $hiera_node['cis_2_1_2']
+  $cis_2_1_3  = $hiera_node['cis_2_1_3']
+  $cis_2_1_4  = $hiera_node['cis_2_1_4']
+  $cis_2_1_5  = $hiera_node['cis_2_1_5']
+  $cis_2_1_10 = $hiera_node['cis_2_1_10']
 
   ## apply rules if xinetd installed
   if ($xinetd_installed == 'true') {
@@ -30,64 +37,66 @@ class cis::trusty64::services::xinetd {
     }
 
     ## CIS 2.1.1 Ensure chargen services are not enabled (Scored)
-    if ($cis_2.1.1) {
+    if ($cis_2_1_1) {
       file { '/etc/xinetd.d/chargen':
         ensure  => present,
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
-        content => dos2unix(template('cis/trusty64/xinetd.d/chargen.erb')),
+        content => dos2unix(template('cis/trusty64/xinetd/chargen.erb')),
+       }
     }
 
     ## CIS 2.1.2 Ensure daytime services are not enabled (Scored)
-    if ($cis_2.1.2) {
+    if ($cis_2_1_2) {
       file { '/etc/xinetd.d/daytime':
         ensure  => present,
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
-        content => dos2unix(template('cis/trusty64/xinetd.d/daytime.erb')),
+        content => dos2unix(template('cis/trusty64/xinetd/daytime.erb')),
+      }
     }
 
     ## CIS 2.1.3 Ensure discard services are not enabled (Scored)
-    if ($cis_2.1.3) {
+    if ($cis_2_1_3) {
       file { '/etc/xinetd.d/discard':
-      ensure  => present,
-      mode    => '0644',
-      owner   => 'root',
-      group   => 'root',
-      content => dos2unix(template('cis/trusty64/xinetd.d/discard.erb')),
+        ensure  => present,
+        mode    => '0644',
+        owner   => 'root',
+        group   => 'root',
+        content => dos2unix(template('cis/trusty64/xinetd/discard.erb')),
+      }
     }
 
     ## CIS 2.1.4 Ensure echo services are not enabled (Scored)
-    if ($cis_2.1.4) {
+    if ($cis_2_1_4) {
       file { '/etc/xinetd.d/echo':
         ensure  => present,
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
-        content => dos2unix(template('cis/trusty64/xinetd.d/echo.erb')),
+        content => dos2unix(template('cis/trusty64/xinetd/echo.erb')),
       }
     }
 
     ## CIS 2.1.5 Ensure time services are not enabled (Scored)
-    if ($cis_2.1.5) {
-      file { '/etc/xinetd.d/echo':
+    if ($cis_2_1_5) {
+      file { '/etc/xinetd.d/time':
         ensure  => present,
         mode    => '0644',
         owner   => 'root',
         group   => 'root',
-        content => dos2unix(template('cis/trusty64/xinetd.d/time.erb')),
+        content => dos2unix(template('cis/trusty64/xinetd/time.erb')),
       }
     }
 
     ## 2.1.10 Ensure xinetd is not enabled (Scored)
-    if ($cis_2.1.10) {
+    if ($cis_2_1_10) {
       service { 'xinetd':
         ensure => false,
         enable => false,
       }
     }
   }
-
 }
