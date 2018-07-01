b3ce8a9eb9630742a46a5797c98ea275dbe86907
jeffrey levesque <jeff1evesque@yahoo.com>
2018-07-01 07:47:13 -0400

    #147: remove 'cis_' file prefix, standardize comments

--- manifests/trusty64/user_settings.pp
+++ manifests/trusty64/user_settings.pp
@@ -1,10 +1,11 @@
+##
 ## CIS Ubuntu 14.04 LTS Server Benchmark
 ## v2.0.0 - 09-30-2016
 ##
 ## https://github.com/jeff1evesque/machine-learning/files/629747/CIS_Ubuntu_Linux_14.04_LTS_Benchmark_v2.0.0.pdf
 ##
 
-class cis::trusty64::cis_user_settings {
+class cis::trusty64::user_settings {
   include cis::trusty64::dependencies
 
   ## local variables: conditionally load hiera
