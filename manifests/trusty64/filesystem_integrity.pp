4d92764adfdab2feaf7fbf432cb16016f957cc88
jeffrey levesque <jeff1evesque@yahoo.com>
2018-07-05 19:34:35 -0400

    #147: filesystem_integrity.pp, dynamically use 'aide'

--- manifests/trusty64/filesystem_integrity.pp
+++ manifests/trusty64/filesystem_integrity.pp
@@ -7,21 +7,39 @@
 
 class cis_benchmark::trusty64::filesystem_integrity {
   ## local variables
-  $exec_path   = $::cis_benchmark::exec_path
-  $report_path = $::cis_benchmark::report_path
+  $timestamp = generate('/bin/date', '+%Y-%d-%m-%H-%M-%S')
 
   ## local variables: stig items
-  $cis_1_3_1   = $::cis_benchmark::cis_1_3_1
+  $cis_1_3_1        = $::cis_benchmark::cis_1_3_1
+  $cis_1_3_2        = $::cis_benchmark::cis_1_3_2
 
   ## 1.3.1 Ensure AIDE is installed (Scored)
-  ## 1.3.2 Ensure filesystem integrity is regularly checked (Scored)
-  ##
-  ## Note: if 1.3.2 is enabled, then the below will remediate the latter.
-  ##
   if ($cis_1_3_1) {
-    aide::watch { 'aide-cis-1-3-1':
-        path   => '/',
-        rules  => 'R'
+    package { 'aide'
+        ensure      => 'installed',
+        notify      => Exec['aide-init']
     }
+
+    exec { 'aide-init':
+        command     => 'aide --init'
+        refreshonly => true,
+    }
+  }
+
+  ## 1.3.2 Ensure filesystem integrity is regularly checked (Scored)
+  if ($cis_1_3_2) {
+      file { '/var/log/aide':
+          ensure => 'directory',
+          owner  => 'root',
+          group  => 'root',
+          mode   => '0700',
+      }
+
+      cron { 'aide-report':
+          command => "/usr/bin/aide --check > /var/log/aide/aidecheck_${timestamp}.txt",
+          user    => 'root',
+          hour    => 5,
+          minute  => 0,
+      }
   }
 }
