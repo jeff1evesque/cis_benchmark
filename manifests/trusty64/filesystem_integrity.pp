bbd32b2f4929c78450620a2964acdd80b527ac0b
jeffrey levesque <jeff1evesque@yahoo.com>
2018-07-07 06:47:51 -0400

    #147: filesystem_integrity.pp, add missing colon

--- manifests/trusty64/filesystem_integrity.pp
+++ manifests/trusty64/filesystem_integrity.pp
@@ -15,7 +15,7 @@ class cis_benchmark::trusty64::filesystem_integrity {
 
   ## 1.3.1 Ensure AIDE is installed (Scored)
   if ($cis_1_3_1) {
-    package { 'aide'
+    package { 'aide':
         ensure      => 'installed',
         notify      => Exec['aide-init']
     }
